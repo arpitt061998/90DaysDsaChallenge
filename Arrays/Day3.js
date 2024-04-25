bed41005f12dbb4f09ce7ab3872bc0198d1fa2f6
Arpit <arpittiwari9898@gmail.com>
2024-04-25 23:26:06 +0530

    commiting Day3 challenge problem(Count pairs with given sum)

---
+++ Arrays/Day3.js
@@ -0,0 +1,56 @@
+/*
+#Count pairs with given sum
+
+Given an array of N integers, and an integer K, find the number of pairs of elements in the array whose sum is equal to K.
+
+
+Example 1:
+
+Input:
+N = 4, K = 6
+arr[] = {1, 5, 7, 1}
+Output: 2
+Explanation: 
+arr[0] + arr[1] = 1 + 5 = 6 
+and arr[1] + arr[3] = 5 + 1 = 6.
+
+Example 2:
+
+Input:
+N = 4, K = 2
+arr[] = {1, 1, 1, 1}
+Output: 6
+Explanation: 
+Each 1 will produce sum 2 with any 1.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function getPairsCount() which takes arr[], n and k as input parameters and returns the number of pairs that have sum K.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+*/
+
+/*Approach
+
+Approach taken is of formula to get 
+  1+2+...+n-1 = nC2
+
+*/
+
+getPairsCount(arr,n,k){
+  var output = 0;
+  var mp = new Map();
+  for(var i=0;i<n;i++){
+    let diff = k-arr[i]
+    if(mp.has(diff)){
+      output+=mp.get(diff);
+    }
+    if(mp.has(arr[i])) {
+      mp.set(arr[i],mp.get(arr[i])+1);
+    } else {
+      mp.set(arr[i],1);
+    }
+  }
+  return output; 
+}
